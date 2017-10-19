f1da327b90aae2ab79b337dd327405a1cce76b85
YanChan  Lin <yonalin1@gmail.com>
2017-10-19 03:33:05 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line(katz_deli)
+puts "The line is currently empty."
+end
+
+def take_a_number (katz_deli,name)
+  counter = 1
+  "Welcome, #{name}. You are number #{position} in line."
+end
+
+def now_serving
+  katz_deli=
+  puts "Currently serving #{name}."
+  if  katz_deli == []
+puts "There is nobody waiting to be served!".
+  end
+ end
+end
+end
