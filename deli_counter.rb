82aa819e4453f2e937288c39038243414f0ee2b3
YanChan  Lin <yonalin1@gmail.com>
2017-10-25 22:43:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,17 +23,11 @@ def take_a_number(katz_deli,name)
 end
 
 def now_serving(katz_deli)
-
-
-  # katz_deli.push(name)
-  #   puts "Currently serving #{name}."
-  # else
-
   if katz_deli == []
     puts "There is nobody waiting to be served!"
   else
 
-    puts "Currently serving #{katz_deli.first}."
+    puts "Currently serving #{katz_deli.first}."+ katz_deli.shift
   end
 end
 end
