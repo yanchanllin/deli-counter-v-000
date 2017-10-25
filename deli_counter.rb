0f92f3756472e6572f334d1cbea524906ea82f0c
YanChan  Lin <yonalin1@gmail.com>
2017-10-25 22:02:58 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,30 @@
+require "pry"
+
 def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   elsif katz_deli.size == 1
     puts "The line is currently: 1. #{katz_deli[0]}"
   elsif katz_deli.size == 2
-    puts "The line is currently: 1. #{katz_deli[0]} 2.#{katz_deli[1]}"
+    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]}"
   else #3 or more people
-      katz_deli.each_with_index do |size, index|
-        puts "The line is currently: 1. #{katz_deli[0]} 2.#{katz_deli[1]} 3.#{katz_deli[2]}"
-  end
+         array = []
+        #  concat
+      #  methods like #push and #join could be useful too
+      katz_deli.each_with_index do |name, index|
+        array.push("#{index+1}. #{name}")
+      end
+    puts "The line is currently: " + array.join(" ")
 end
 
-def take_a_number (katz_deli,name)
-  katz_deli.each_with_index do |name, position|
-  puts "Welcome, #{name}. You are number #{position} in line."
-  end
+def take_a_number(katz_deli,name)
+   if katz_deli == []
+     katz_deli.push(name)
+  # katz_deli.each_with_index do |name, index|
+  puts "Welcome, #{name}. You are number #{1} in line."
+
+   end
+
 end
 
 def now_serving(katz_deli)
